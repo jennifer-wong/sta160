1339394a27d16a35b24970d7887f28b68d45bb0a
mrdallasfuge <49919397+mrdallasfuge@users.noreply.github.com>
2019-06-01 15:48:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
+
 
 movies = pd.read_csv('data/AllMoviesDetailsCleaned.csv', sep=';', low_memory=False)
 cast = pd.read_csv('data/AllMoviesCastingRaw.csv', sep=';', low_memory=False)
